f6c8ec4e2eef33ef888da1e305038424a0e096d3
Rans4ckeR <ransacker@outlook.com>
2025-06-20 13:55:27 +0200

    Update project

--- Properties/GlobalUsings.cs
+++ Properties/GlobalUsings.cs
@@ -1,5 +1,8 @@
 ﻿#pragma warning disable SA1200 // Using directives should be placed correctly
 global using System.Net;
 global using System.Net.Sockets;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Hosting;
+global using Microsoft.Extensions.Logging;
 global using Microsoft.Extensions.Options;
 #pragma warning restore SA1200 // Using directives should be placed correctly
